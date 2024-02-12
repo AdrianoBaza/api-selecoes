953984e573953cf6ae2ce11d64675e390a5743e7
AdrianoBaza <bazafordev@gmail.com>
2024-02-12 14:59:51 -0300

    refactor: persistencia-com-mysql
    habilita persistência com mysql; refatora o server.js e o app.js

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 import app from './src/app.js'
 
-import conexao from './infra/conexao.js'
-
 const PORT = 3000
 
-conexao.connect((erro) => {
-    if(erro) {
-        console.log(erro)
-    } else {
-        console.log('Conexão realizada com sucesso!')
-        // escutar a porta 3000
-        app.listen(PORT, () => {
-            console.log(`O servidor está rodando no endereço http://localhost:${PORT}`)
-        })
-    }
+ // escutar a porta 3000
+ app.listen(PORT, () => {
+    console.log(`O servidor está rodando no endereço http://localhost:${PORT}`)
 })
--- src/app.js
+++ src/app.js
@@ -1,62 +1,77 @@
 import express from 'express'
+import conexao from '../infra/conexao.js'
 
 const app = express()
 
 // indicar para o express ler body com json
 app.use(express.json())
 
-// mock
-const selecoes = [
-    {id: '1', selecao: 'Brasil', grupo: 'G'},
-    {id: '2', selecao: 'Suíça', grupo: 'G'},
-    {id: '3', selecao: 'Camarões', grupo: 'G'},
-    {id: '4', selecao: 'Sérvia', grupo: 'G'}
-]
-
-// função para buscar seleção por id
-function buscarSelecaoPorID(id) {
-    return selecoes.filter(selecao => selecao.id == id)
-}
-
-// função para pegar o índice do elemento no array seleções
-function buscarIndexSelecao(id) {
-    return selecoes.findIndex(selecao => selecao.id == id)
-}
-
 // rota para cadastrar uma seleção
 app.post('/selecoes', (req, res) => {
-    selecoes.push(req.body)
-    res.status(201).send('Seleção cadastrada com sucesso!')
-})
-
-// rota padrão
-app.get('/', (req, res) => {
-    res.send('Curso de Node JS')
+    const selecao = req.body
+    const sql = "INSERT INTO selecoes SET ?;"
+    conexao.query(sql, selecao, (erro, resultado) => {
+        const linha = resultado[0]
+        if(erro) {
+           res.status(404).json( {'erro': erro} )
+        } else {
+            res.status(201).json(resultado)
+        }
+    })
 })
 
 // rota para listar todas as seleções
 app.get('/selecoes', (req, res) => {
-    res.send(selecoes)
+    const sql = "SELECT * FROM selecoes;"
+    conexao.query(sql, (erro, resultado) => {
+        if(erro) {
+           res.status(404).json( {'erro': erro} )
+        } else {
+            res.status(200).json(resultado)
+        }
+    })
 })
 
 // rota para listar seleção por ID
 app.get('/selecoes/:id', (req, res) => {
-    res.status(200).json(buscarSelecaoPorID(req.params.id))
+    const id = req.params.id
+    const sql = "SELECT * FROM selecoes WHERE id=?;"
+    conexao.query(sql, id, (erro, resultado) => {
+        const linha = resultado[0]
+        if(erro) {
+           res.status(404).json( {'erro': erro} )
+        } else {
+            res.status(200).json(linha)
+        }
+    })
 })
 
 // rota para atualizar seleção por ID
 app.put('/selecoes/:id', (req, res) => {
-    let index = buscarIndexSelecao(req.params.id)
-    selecoes[index].selecao = req.body.selecao
-    selecoes[index].grupo   = req.body.grupo
-    res.json(selecoes[index])
+    const id = req.params.id
+    const selecao = req.body
+    const sql = "UPDATE selecoes SET ? WHERE id=?;"
+    conexao.query(sql, [selecao, id], (erro, resultado) => {
+        const linha = resultado[0]
+        if(erro) {
+           res.status(404).json( {'erro': erro} )
+        } else {
+            res.status(200).json(resultado)
+        }
+    })
 })
 
 // rota para deletar seleção por ID
 app.delete('/selecoes/:id', (req, res) => {
-    const index = buscarIndexSelecao(req.params.id)
-    selecoes.splice(index, 1)
-    res.status(200).send(`Seleção com id ${req.params.id} excluída com sucesso`)
+    const id = req.params.id
+    const sql = "DELETE FROM selecoes WHERE id=?;"
+    conexao.query(sql, id, (erro, resultado) => {
+        if(erro) {
+           res.status(404).json( {'erro': erro} )
+        } else {
+            res.status(200).json(resultado)
+        }
+    })
 })
 
 export default app
