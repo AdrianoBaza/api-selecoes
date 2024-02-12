e3d105dca264aa418f44ba7673d08fd2626b4105
AdrianoBaza <bazafordev@gmail.com>
2024-02-12 12:57:17 -0300

    feat: rota update
    insere rota update com seleção por id

--- src/app.js
+++ src/app.js
@@ -23,6 +23,12 @@ function buscarIndexSelecao(id) {
     return selecoes.findIndex(selecao => selecao.id == id)
 }
 
+// rota para cadastrar uma seleção
+app.post('/selecoes', (req, res) => {
+    selecoes.push(req.body)
+    res.status(201).send('Seleção cadastrada com sucesso!')
+})
+
 // rota padrão
 app.get('/', (req, res) => {
     res.send('Curso de Node JS')
@@ -38,10 +44,12 @@ app.get('/selecoes/:id', (req, res) => {
     res.status(200).json(buscarSelecaoPorID(req.params.id))
 })
 
-// rota para cadastrar uma seleção
-app.post('/selecoes', (req, res) => {
-    selecoes.push(req.body)
-    res.status(201).send('Seleção cadastrada com sucesso!')
+// rota para atualizar seleção por ID
+app.put('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes[index].selecao = req.body.selecao
+    selecoes[index].grupo   = req.body.grupo
+    res.json(selecoes[index])
 })
 
 // rota para deletar seleção por ID
