459a0e84ca76f75aab92cd521680da924312ee95
AdrianoBaza <bazafordev@gmail.com>
2024-02-12 15:45:27 -0300

    refactor: mvc-controller e ajustes

--- src/app.js
+++ src/app.js
@@ -1,77 +1,16 @@
 import express from 'express'
-import conexao from '../infra/conexao.js'
+import SelecaoController from './app/controllers/SelecaoController.js'
 
 const app = express()
 
 // indicar para o express ler body com json
 app.use(express.json())
 
-// rota para cadastrar uma seleção
-app.post('/selecoes', (req, res) => {
-    const selecao = req.body
-    const sql = "INSERT INTO selecoes SET ?;"
-    conexao.query(sql, selecao, (erro, resultado) => {
-        const linha = resultado[0]
-        if(erro) {
-           res.status(404).json( {'erro': erro} )
-        } else {
-            res.status(201).json(resultado)
-        }
-    })
-})
-
-// rota para listar todas as seleções
-app.get('/selecoes', (req, res) => {
-    const sql = "SELECT * FROM selecoes;"
-    conexao.query(sql, (erro, resultado) => {
-        if(erro) {
-           res.status(404).json( {'erro': erro} )
-        } else {
-            res.status(200).json(resultado)
-        }
-    })
-})
-
-// rota para listar seleção por ID
-app.get('/selecoes/:id', (req, res) => {
-    const id = req.params.id
-    const sql = "SELECT * FROM selecoes WHERE id=?;"
-    conexao.query(sql, id, (erro, resultado) => {
-        const linha = resultado[0]
-        if(erro) {
-           res.status(404).json( {'erro': erro} )
-        } else {
-            res.status(200).json(linha)
-        }
-    })
-})
-
-// rota para atualizar seleção por ID
-app.put('/selecoes/:id', (req, res) => {
-    const id = req.params.id
-    const selecao = req.body
-    const sql = "UPDATE selecoes SET ? WHERE id=?;"
-    conexao.query(sql, [selecao, id], (erro, resultado) => {
-        const linha = resultado[0]
-        if(erro) {
-           res.status(404).json( {'erro': erro} )
-        } else {
-            res.status(200).json(resultado)
-        }
-    })
-})
-
-// rota para deletar seleção por ID
-app.delete('/selecoes/:id', (req, res) => {
-    const id = req.params.id
-    const sql = "DELETE FROM selecoes WHERE id=?;"
-    conexao.query(sql, id, (erro, resultado) => {
-        if(erro) {
-           res.status(404).json( {'erro': erro} )
-        } else {
-            res.status(200).json(resultado)
-        }
-    })
-})
+// ROTAS
+app.post('/selecoes', SelecaoController.store)
+app.get('/selecoes', SelecaoController.index)
+app.get('/selecoes/:id', SelecaoController.show)
+app.put('/selecoes/:id', SelecaoController.update)
+app.delete('/selecoes/:id', SelecaoController.delete)
 
 export default app
