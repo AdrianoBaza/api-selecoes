04631d268f12c1b3c8dc40475aad6070a58f29b8
AdrianoBaza <bazafordev@gmail.com>
2024-02-12 01:56:52 -0300

    refactor: app
    cria server.js separa rotas e servidor

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
-const express = require('express')
+import express from 'express'
+
 const app = express()
-const port = 3000
 
 // cria a rota padrão
 app.get('/', (req, res) => {
     res.send('Curso de Node JS')
 })
 
-// escutar a porta 3000
-app.listen(port, () => {
-    console.log(`O servidor está rodando no endereço http://localhost:${port}`)
-})
+export default app
